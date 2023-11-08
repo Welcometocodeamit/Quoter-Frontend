b9fa21fd30f3f4750ed8f7400a47d5294a168fcb
Welcometocodeamit <kumbharamit98@gmail.com>
2023-11-08 21:11:48 +0530

    Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './Footer.css'
-import heart from '../../Assets/heart.png'
 import copyright from '../../Assets/copyright.png'
 
 export default function Footer() {
   return (
     <div id="footer">
-    <h6><img src={copyright} alt="copyright"/>  2023  |  Designed and coded with <img src={heart} alt="heart"/> by Amit Kumbhar</h6>
+    <h6><img src={copyright} alt="copyright"/>  2023  |  Designed and coded by Amit Kumbhar</h6>
     </div>
   )
 }
